6044533c52a847629efbe1b78edc7bf010f5a32e
benshaw3283 <38153391+benshaw3283@users.noreply.github.com>
2024-04-22 20:03:47 +1000

    Done - just make look good

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 "use client";
-import React from "react";
+import Tutorial from "@/components/Tutorial";
+import React, { MutableRefObject, useEffect } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 interface Reaction {
   reaction: string;
   actor: string;
+}
+
+interface Participant {
+  name: string;
 }
 
 interface Message {
@@ -14,41 +26,17 @@ interface Message {
   is_geoblocked_for_viewer: boolean;
 }
 
-interface File {
-  name: string;
-  type: string;
-  size: number;
-}
-
 export default function Home() {
   const [selectedFiles, setSelectedFiles] = React.useState<File[]>([]);
+  const [fileMessages, setFileMessages] = React.useState<Message[]>([]);
+  const [participants, setParticipants] = React.useState<Participant[]>([]);
+  const [selectedValue, setSelectedValue] = React.useState<number>(0);
 
-  selectedFiles.forEach((file, index) => {
-    const currentFile = selectedFiles[index];
+  const handleChange = (value: string) => {
+    setSelectedValue(parseInt(value));
+  };
 
-    // Create a new FileReader object
-    const reader = new FileReader();
-
-    // Define an event handler for when the file reading is complete
-    reader.onload = (event: any) => {
-      // Access the file contents from the event target
-      const fileContents = event.target.result;
-
-      // Parse the file contents as JSON
-      try {
-        const jsonData = JSON.parse(fileContents);
-
-        console.log(jsonData.messages.length); // This will log the parsed JSON data to the console
-      } catch (error) {
-        console.error("Error parsing JSON:", error);
-      }
-    };
-
-    // Read the file as text
-    reader.readAsText(currentFile);
-  });
-
-  const handleFileSelect = (event: any) => {
+  const handleFileSelect = async (event: any) => {
     const files = event.target.files; // Access the FileList object
 
     // Convert the FileList object to an array
@@ -58,32 +46,245 @@ export default function Home() {
     setSelectedFiles(fileListArray);
   };
 
+  const readParticipants = async () => {
+    try {
+      // Find the file object for message_1.json
+      const message1File = selectedFiles.find(
+        (file) => file.name === "message_1.json"
+      );
+      if (!message1File) {
+        console.error("message_1.json not found in selected files.");
+        return;
+      }
+
+      const reader = new FileReader();
+      const blob = new Blob([message1File], { type: message1File.type });
+
+      reader.onload = (event: any) => {
+        const fileContents = event.target.result;
+        try {
+          const jsonData = JSON.parse(fileContents);
+          const participantsArray = jsonData.participants;
+          setParticipants(participantsArray);
+          console.log("Participants array:", participantsArray);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      };
+
+      reader.readAsText(blob);
+    } catch (error) {
+      console.error("Error reading file:", error);
+    }
+  };
+
+  const readThenSpread = async () => {
+    selectedFiles.forEach((file, index) => {
+      const currentFile = selectedFiles[index];
+      const reader = new FileReader();
+
+      // Create a Blob object from the File object
+      const blob = new Blob([currentFile], { type: currentFile.type });
+
+      reader.onload = (event: any) => {
+        const fileContents = event.target.result;
+
+        try {
+          const jsonData = JSON.parse(fileContents);
+          const spread: any = [...jsonData.messages];
+          setFileMessages((prev) => [...prev, ...spread]);
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
+      };
+
+      // Pass the Blob object to readAsText()
+      reader.readAsText(blob);
+    });
+  };
+
+  const messageCounts = participants.map((participant) => {
+    const count = fileMessages?.filter(
+      (message) => message.sender_name === participant.name
+    ).length;
+    return { name: participant.name, count: count };
+  });
+
+  const sortedCount = messageCounts.sort((a, b) => b.count - a.count);
+
+  const numberReactions = (number: number) => {
+    // Function to find messages with 10 or more reactions
+    const findMessagesWithNumberPlusReactions = (messages: any) => {
+      const messagesWithNumberPlusReactions = messages.filter(
+        (message: Message) =>
+          message?.reactions && message.reactions.length >= number
+      );
+      return messagesWithNumberPlusReactions;
+    };
+
+    // Get messages with 10 or more reactions
+    const messagesWithNumberPlusReactions =
+      findMessagesWithNumberPlusReactions(fileMessages);
+
+    // Create a map to count messages by sender
+    const messagesCountBySender: any = {};
+    messagesWithNumberPlusReactions.forEach((message: Message) => {
+      const senderName = message.sender_name;
+      if (!messagesCountBySender[senderName]) {
+        messagesCountBySender[senderName] = 1;
+      } else {
+        messagesCountBySender[senderName]++;
+      }
+    });
+
+    // Convert messagesCountBySender to an array for sorting
+    const senderList = Object.entries(messagesCountBySender);
+
+    // Sort senderList by message count
+    senderList.sort((a: any, b: any) => b[1] - a[1]);
+
+    // Render the sorted list of senders and their message counts
+    const sortedSenderList = senderList.map(([senderName, messageCount]) => (
+      <p key={senderName}>{`${senderName}: ${messageCount}`}</p>
+    ));
+
+    return sortedSenderList;
+  };
+
+  // Function to find the user who reacted the most
+  const findReactedMost = (messages: Message[]) => {
+    const reactionCounts: any = {};
+    messages.forEach((message) => {
+      if (message.reactions) {
+        message.reactions.forEach((reaction) => {
+          const user = reaction.actor;
+          if (!reactionCounts[user]) {
+            reactionCounts[user] = 1;
+          } else {
+            reactionCounts[user]++;
+          }
+        });
+      }
+    });
+
+    let mostReactedUser = null;
+    let maxReactions = 0;
+    for (const [user, count] of Object.entries(reactionCounts)) {
+      if (count > maxReactions) {
+        mostReactedUser = user;
+        maxReactions = count;
+      }
+    }
+
+    return mostReactedUser;
+  };
+
+  // Find the user who reacted the most
+  const reactedMost = findReactedMost(fileMessages);
+
+  React.useEffect(() => {
+    readThenSpread();
+    readParticipants();
+  }, [selectedFiles]);
+
   return (
-    <main>
-      <div className="flex flex-col container">
-        <h1 className="flex self-center font-bold text-3xl pt-4">
+    <main className="bg-black text-white min-h-screen">
+      <div className="flex flex-col container items-center">
+        <h1 className="flex self-center font-bold text-3xl py-4">
           Facebook Messenger Statistics
         </h1>
 
-        <div>
-          {/* @ts-expect-error */}
+        <div className="">
           <input
             type="file"
             directory=""
             webkitdirectory=""
             multiple
             onChange={handleFileSelect}
+            className=""
           />
         </div>
-        <div>
-          <h2>Selected Files:</h2>
-          <ul>
-            {selectedFiles?.map((file, index) => (
-              <li key={index}>{file.name}</li>
-            ))}
-          </ul>
-        </div>
       </div>
+      {selectedFiles.length ? (
+        <div className="flex flex-col container items-center">
+          <div className="flex flex-row gap-2 py-3 text-xl font-semibold">
+            <p>Total Messages:</p>
+            <p>{fileMessages?.length}</p>
+          </div>
+          <div>
+            <p className="text-base font-semibold">{`Reacted the most: ${reactedMost}`}</p>
+          </div>
+          <div className="pt-5">
+            <h2 className="font-semibold text-xl border-b-2 flex justify-center">
+              Messages Sent
+            </h2>
+            <div className="py-3">
+              {sortedCount.map((person, index) => (
+                <p key={index}>{`${person.name}: ${person.count}`}</p>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className="flex flex-row border-b-2">
+              <h2 className="font-semibold text-xl  pb-1 flex justify-center pr-1">
+                Messages with
+              </h2>
+              <Select onValueChange={(v) => handleChange(v)}>
+                <SelectTrigger className="w-[60px] h-[25px] text-black">
+                  <SelectValue placeholder="1" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="1">1</SelectItem>
+                  <SelectItem value="2">2</SelectItem>
+                  <SelectItem value="3">3</SelectItem>
+                  <SelectItem value="4">4</SelectItem>
+                  <SelectItem value="5">5</SelectItem>
+                  <SelectItem value="6">6</SelectItem>
+                  <SelectItem value="7">7</SelectItem>
+                  <SelectItem value="8">8</SelectItem>
+                  <SelectItem value="9">9</SelectItem>
+                  <SelectItem value="10">10</SelectItem>
+                  <SelectItem value="11">11</SelectItem>
+                  <SelectItem value="12">12</SelectItem>
+                  <SelectItem value="13">13</SelectItem>
+                  <SelectItem value="14">14</SelectItem>
+                  <SelectItem value="15">15</SelectItem>
+                  <SelectItem value="16">16</SelectItem>
+                  <SelectItem value="17">17</SelectItem>
+                  <SelectItem value="18">18</SelectItem>
+                  <SelectItem value="19">19</SelectItem>
+                  <SelectItem value="20">20</SelectItem>
+                  <SelectItem value="21">21</SelectItem>
+                  <SelectItem value="22">22</SelectItem>
+                  <SelectItem value="23">23</SelectItem>
+                  <SelectItem value="24">24</SelectItem>
+                  <SelectItem value="25">25</SelectItem>
+                  <SelectItem value="26">26</SelectItem>
+                  <SelectItem value="27">27</SelectItem>
+                  <SelectItem value="28">28</SelectItem>
+                  <SelectItem value="29">29</SelectItem>
+                  <SelectItem value="30">30</SelectItem>
+                </SelectContent>
+              </Select>
+              <p className="font-semibold text-xl  pb-1 flex justify-center pl-1">
+                + Reactions
+              </p>
+            </div>
+            <div className="py-3">
+              {" "}
+              {fileMessages ? numberReactions(selectedValue) : ""}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <p className="flex text-lg font-semibold justify-center pt-4">
+            Upload files to see statistics
+          </p>
+        </div>
+      )}
+
+      <Tutorial />
     </main>
   );
 }
---
+++ components/Tutorial.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+import Image from "next/image";
+
+const Tutorial = () => {
+  return (
+    <div className="pt-10">
+      <h1 className="flex justify-center text-4xl font-semibold">Tutorial</h1>
+      <div className="border border-slate-300 w-full"></div>
+      <div className="flex flex-col pl-3">
+        <div className="pt-2">
+          <h1 className="text-2xl font-semibold border-b w-fit border-slate-300">
+            Step 1
+          </h1>
+          <p>Go on Facebook and click your profile picture in the top right.</p>
+          <p>
+            Click <strong>Settings & Privacy</strong>
+          </p>
+
+          <p className="flex flex-row gap-[18px]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+            <strong>Settings</strong>
+          </p>
+          <p>
+            Then scroll down the left and click on{" "}
+            <strong>Download your information</strong>
+          </p>
+          <Image
+            src="/step1.4.PNG"
+            alt="step1.3"
+            width={300}
+            height={300}
+            className="pl-1"
+          />
+          <p className="pt-2">
+            Enter your password and then click the{" "}
+            <strong>Download or transfer information</strong> button shown below
+          </p>
+          <Image src="/step1.6.PNG" alt="step" width={300} height={300} />
+          <p className="pt-2">
+            Then select <strong>Specific types of information</strong> as shown
+            below
+          </p>
+          <Image src="/step1.7.PNG" alt="step" width={300} height={300} />
+          <p className="pt-2">
+            Select <strong>Messages</strong> and click <strong>next</strong>
+          </p>
+          <Image src="/step1.8.PNG" alt="step" width={300} height={300} />
+          <p className="pt-2">
+            Press <strong>Next</strong>, selecting{" "}
+            <strong>Download to device</strong> by default
+          </p>
+          <p className="pt-3">
+            On this popup, make sure to change Date range to{" "}
+            <strong>All time</strong> and Format to <strong>JSON</strong>
+          </p>
+          <Image src="/step1.9.PNG" alt="step" width={300} height={300} />
+          <p className="pt-4">
+            Click <strong>Create files</strong> and wait approximately 1 day for
+            the files to be ready to download
+          </p>
+        </div>
+        <div className="pt-4">
+          <h2 className="text-2xl font-semibold border-b border-slate-300 w-fit">
+            Step 2
+          </h2>
+          <p>
+            When your files are ready to download there should be multiple files
+            available to download. For me, the file containing the messages was{" "}
+            <strong>3/5</strong>, but it may vary.
+          </p>
+          <p className="pt-3">
+            Open the zip folder, it should be named something like :
+            facebook-your-name19375
+          </p>
+          <p className="pt-2">Follow this order</p>
+          <div className="flex flex-row">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+            <p className="font-semibold pl-1">your_facebook_activity</p>
+          </div>
+          <div className="flex flex-row pl-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+            <p className="font-semibold pl-1">messages</p>
+          </div>
+          <div className="flex flex-row pl-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={2}
+              stroke="currentColor"
+              className="w-6 h-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+              />
+            </svg>
+            <p className="font-semibold pl-1">inbox</p>
+          </div>
+          <p>
+            Find the folder with the name of the conversation you want to see
+            the stats of and put it on your desktop
+          </p>
+          <p className="py-3">
+            Then, click choose files at the top of this page and select that
+            folder - and there you go!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tutorial;
